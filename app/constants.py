9033f44293005bf003b1d0a02c5bea2bf3da1757
Kenta Terada <k.terada.1211@gmail.com>
2025-10-02 22:00:00 +0900

    Add constants.py

---
+++ app/constants.py
@@ -0,0 +1,17 @@
+TICKER_NAME_TO_SYMBOL = {
+    "S&P500": "^GSPC",
+    "NASDAQ100": "^NDX",
+    "QLD": "QLD",
+    "SOXL": "SOXL",
+    "BTC": "BTC-USD",
+    "ETH": "ETH-USD",
+}
+
+TICKER_NAME_TO_START_YEAR = {
+    "S&P500": 1928,
+    "NASDAQ100": 1985,
+    "QLD": 2006,
+    "SOXL": 2010,
+    "BTC": 2014,
+    "ETH": 2017,
+}

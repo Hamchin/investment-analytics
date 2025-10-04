59cfa9b510e32dacd962c8b7c746c63bf4c7e225
Kenta Terada <k.terada.1211@gmail.com>
2025-10-04 11:00:00 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ import streamlit as st
 
 pages = [
     st.Page("pages/top.py", title="トップ", icon=":material/home:"),
-    st.Page("pages/history.py", title="時系列分析", icon=":material/analytics:"),
+    st.Page("pages/realtime.py", title="リアルタイム分析", icon=":material/show_chart:"),
+    st.Page("pages/history.py", title="時系列分析", icon=":material/finance:"),
 ]
 
 page = st.navigation(pages)
